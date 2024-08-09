d8e23e02d31f7e18cfbc8f58d9514e081f452a28
jared6600cc <135299193+jared6600cc@users.noreply.github.com>
2024-08-08 22:03:20 -0500

    Reduced complexity / Added robustness

--- 0_helloWorld/helloWorld.c
+++ 0_helloWorld/helloWorld.c
@@ -2,22 +2,19 @@
 #include <stdio.h>
 #include <string.h>
 
-int screenTileHeight = 18;
-int screenTileWidth = 20;
-char * mssg = "Hello World";
-
-
-void printMiddleScreen(){
-	int i;
-	for(i=0; i<(screenTileHeight/2); i++){
-		printf(" \n");
-	}
-	for(i=0; i<((strlen(mssg)-1)/2); i++){
-		printf(" ");
-	}
-	printf("%s", mssg);
-}
-
 void main(){
-	printMiddleScreen();
+	// Init device
+	display_off();	// off for setup
+	set_mode(0);	// 0= textmode
+	
+	// Print to screen	
+	printf("\nHello World\n");
+	
+	// Turn on display
+	display_on();
+	
+	// Main game loop
+	while(1){
+		wait_vbl_done();		// avoid screen tear
+	}
 }
